d3b4490fa6bd81df1a24d7c00cf28e7df734bdfb
DeepRulz <108786057+DeepRulz@users.noreply.github.com>
2025-06-15 01:23:09 +0530

    Update final.py

--- final.py
+++ final.py
@@ -92,7 +92,7 @@ if query:
 
 st.markdown("""
     <div class="footer">
-        Created by <a href="https://github.com/DeepRulz" target="_blank">Deep Shah</a> and <a href="https://github.com/Rihaan-Shaikh" target="_blank">Rihaan Sheikh</a>
+        Created by <a href="https://github.com/DeepRulz" target="_blank">Deep Shah</a> and <a href="https://github.com/Rihaan-Shaikh" target="_blank">Rihaan Shaikh</a>
     </div>
 """, unsafe_allow_html=True)
 
